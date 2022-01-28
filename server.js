7425698641a8bd0a916f19cccc930a588f54bdd1
Abdul-Wahab-318 <wahabmaliq@gmail.com>
2022-01-28 12:59:33 +0500

    whitelists kfc-admin

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cookieParser())
 let cors = require("cors")
 app.use(cors({
     origin:['http://localhost:3001' , 'http://localhost:3000' 
-    , "https://kfc-awm.netlify.app" , "https://kfc-admin.netlify.app/"],
+    , "https://kfc-awm.netlify.app" , "https://kfc-admin.netlify.app"],
     credentials: true
 }))
 
